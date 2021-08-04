c98e493fb3140014da77f23b92f331dcd3fb3127
ayhanunal <unalayhan@yahoo.com>
2021-08-04 20:41:30 +0300

    threshold, velocity definitions were made and gestureDetector infrastructure was established

---
+++ app/src/main/java/com/ayhanunal/swipetouchlistener/listeners/OnSwipeTouchListener.kt
@@ -0,0 +1,34 @@
+package com.ayhanunal.swipetouchlistener.listeners
+
+import android.content.Context
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import kotlin.math.abs
+
+
+open class OnSwipeTouchListener(ctx: Context) : View.OnTouchListener {
+
+    private val SWIPE_THRESHOLD = 100
+    private val SWIPE_VELOCITY_THRESHOLD = 100
+    private val gestureDetector: GestureDetector
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    private inner class GestureListener : SimpleOnGestureListener() {
+
+    }
+    open fun onDoubleTap(){}
+    open fun onSwipeRight() {}
+    open fun onSwipeLeft() {}
+    open fun onSwipeTop() {}
+    open fun onSwipeBottom() {}
+
+    init {
+        gestureDetector = GestureDetector(ctx, GestureListener())
+    }
+
+}
